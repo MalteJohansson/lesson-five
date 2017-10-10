ffc79b6104d3a412dc39d60b43824a3b1dec66e1
Malte Johansson <malte.johansson@gmail.com>
2017-10-10 12:13:30 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
